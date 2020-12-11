81e23f5d47d2d4ec35816222b628b885a856b021
Nicolas-Pietropaolo <70975475+Nicolas-Pietropaolo@users.noreply.github.com>
2020-12-10 20:27:47 -0500

    Update Q1-CProgramming.c

--- Q1-CProgramming.c
+++ Q1-CProgramming.c
@@ -3,9 +3,21 @@
  [30 Marks]
 In this question, you are required to implement a linked list in C
 The question has four parts, answer all of them in this same file*/
+
+
+	
+	
+	
+	
  
 /*(a) [5 Marks] write structure Node that holds an integer value as well as a pointer to the next node*/
 //Answer:
+
+typedef struct {//define structure
+	int val; //define value
+	struct Node* nextNode; 
+} Node;
+	//holds pointer to the next node
 
 
 
@@ -17,7 +29,9 @@ The question has four parts, answer all of them in this same file*/
 //Answer:
 
 
-
+typedef struct {//define structure
+	struct Node* firstNodehead; //pointer of first node in list
+}SLL;
 
 
 
@@ -30,6 +44,21 @@ returns:
 //Answer:
 
 
+int searchList (Node* firstNodehead, int targetVal){ //define function searchList
+	
+	while(firstNodehead!= NULL){
+		if(firstNodehead->val == targetVal){
+			return 1; //returns 1 if target is found in list
+		}
+		
+		firstNodehead = firstNodehead->nextNode;
+	}//while loop implemented to see if target value is found in the list and to go through all nodes in list
+	
+	return 0;
+	
+}
+	
+
 
 
   
@@ -41,16 +70,61 @@ the correctness of the returned value from the function by comparing it against 
 int main(){
 	
 	
+	Node**nodeList;
+	
+	nodeList = calloc(10, sizeof(Node *)); //allocate memory of list of 10 nodes
+	
+	int a;
+	
+	for(int a =0; a<10;a++){
+		nodeList[a] = malloc(sizeof(Node));
+		nodeList[a]->val = a;
+		nodeList[a]->nextNode = NULL;
+	}//use malloc as amount of values is unknown
 	
 	
+	for(int b=0; b<9;b++){
+		nodeList[b] -> nextNode = nodeList[b+1];
+		
+	}
 	
+	SLL *SList = nodeList[0];
 	
-	
-	
-	
-	
-	
-	
-	
-	return 0;
+    //create test cases to test function
+    	int caseOne = searchList (SList, 22);
+    	int caseTwo = searchList (SList, 27);
+    
+    	printf("caseOne equals 0; testOne = %d. \n testTwo equals 0; testTwo = %d",caseOne,caseTwo); //print test cases
+    	
+    	for (int c=0; c<10;c++){
+        	free(nodeList[a]); //for loop implemented to ensure all memory is deallocated
+    }
+    free(nodeList);//deallocate memory
+    
+    return 0;
 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
