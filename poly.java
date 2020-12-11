406675d441bd9e857c95a9c6d2bfd5986e9af03e
Nicolas-Pietropaolo <70975475+Nicolas-Pietropaolo@users.noreply.github.com>
2020-12-10 19:16:11 -0500

    Update poly.java

--- poly.java
+++ poly.java
@@ -1,7 +1,6 @@
 /* Question 2
 [Java Programming]
 [20 Marks]
-
 Inheritance and Polymorphism
 Consider the following classes:
 */
@@ -49,67 +48,93 @@ Creature var6 = new Creature();
 For each of the following lines/statements, indicate the output produced by the statement in the line below the statement. 
 If the statement produces more than one line of output, write each line of output in a seprate line 
 If the statement causes an error, write the phrase ”error” to indicate this.
-
-
 NOTE that this is a code snippet and not a complete java program; hence, you do not need to compile or run it
 */
 
 var1.method2();
-//answer:
+//answer: 
+Mammal 2
 
 var2.method2();
-//answer:
+//answer: 
+Horse 2
 
 var3.method2();
-//answer:
+//answer: 
+Ponny 2
+Mammal 2
 
 var4.method2();
-//answer:
+//answer: 
+Horse 2 
 
 var5.method2();
-//answer:
-
+//answer: 
+error
+ 
 var6.method2();
-//answer:
+//answer: 
+Creature 2
 
 var1.method3();
-//answer:
+//answer: 
+Mammal 2
+Mammal 3
 
 var2.method3();
-//answer:
+//answer: 
+error
 
 var3.method3();
 //answer:
+Ponny 2
+Mammal 2
+Mammal 3
 
 var4.method3();
 //answer:
+Horse 2 
+Mammal 3
 
 ((Ponny) var1).method1();
 //answer:
+error
 
 ((Horse) var2).method1();
 //answer:
+Horse 1
 
 ((Ponny) var3).method1();
-//answer:
+//answer: 
+Ponny 1
 
 ((Ponny) var4).method1();
-//answer:
+//answer: 
+error
 
 ((Horse) var4).method2();
 //answer:
+Horse 2
 
 ((Mammal) var5).method2();
-//answer:
+//answer: 
+Mammal 2
 
 ((Creature) var5).method3();
 //answer:
+error
 
 ((Horse) var2).method3();
 //answer:
+Horse 2
+Mammal 3
 
 ((Ponny) var3).method3();
 //answer:
+Ponny 2
+Mammal 2
+Mammal 3
 
 ((Ponny) var5).method3();
 //answer:
+error
